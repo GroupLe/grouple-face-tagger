6444ca13d5fce040701ef40f1f4de915557abc4f
Pinchuk Maya <mayaprr@yandex.ru>
2021-07-07 14:54:28 +0300

    delete lstm

--- models/NER/datasets/reader.py
+++ models/NER/datasets/reader.py
@@ -4,8 +4,7 @@ def read_conll(path):
     """
     Returns list of sentences. Sentence is a list of pairs (word, NE-label)
     """
-    data = open(path)
-    data = data.read().split('\n\n')[1:]
+    data = open(path).read().split('\n\n')[1:]
     data = list(map(str.splitlines, data))
     get_token_and_ne = lambda s: (s.split()[0], s.split()[-1])
     data = list(map(lambda sent: list(map(get_token_and_ne, sent)), data))
